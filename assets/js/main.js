5323e4ab54856197be0e90d2b9b3f8fbbf438844
valentina lopez <valentina lopez>
2017-05-24 23:08:32 -0400

    funcion validadora para todos los campos y eventListener

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,52 @@ hackathon.addEventListener("click",function(){
 	document.getElementById("love").style.float="right";
 });
 
-var name=document.getElementById("name").value;
-var email=document.getElementById("email").value;
-var subject=document.getElementById("subject").value;
-var message=document.getElementById("message").value;
-var send=document.getElementById("submit");
-send.addEventListener("click",validate);
+
 /*funcion validadora de todos los campos*/
+
+var send= document.getElementById("submit");
+send.addEventListener("click", validate);
+
 function validate(){
-	
+		function nameForm(){
+		var inputName= document.getElementById("name").value;
+		if(inputName.length==0){
+			alert("Ingresa tu nombre");
+		}
+		else if(inputName.charAt(0) != inputName.charAt(0).toUpperCase()){
+			alert("Escribe la letra inicial de tu nombre con mayúscula");
+		}
+	}
+	nameForm();
+
+	function emailForm(){
+		var inputEmail=document.getElementById("email").value;
+		var validEmail= /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(inputEmail.length==0){
+			alert("Ingresa tu e-mail")
+		}
+		else if(!validEmail.test(inputEmail)){
+			alert("E-mail inválido, inténtalo de nuevo")
+		}
+	}
+	emailForm();
+
+	function subjectForm(){
+		var inputSubject=document.getElementById("subject").value;
+		if(inputSubject.length==0){
+			alert("Ingresa tu asunto");
+		}
+	}
+	subjectForm();
+
+	function messageForm(){
+		var inputMessage = document.getElementById("message").value;
+		if(inputMessage.length == 0){
+			alert("Ingresa un mensaje")
+		}
+		else if(inputMessage.length>=100){
+			alert("Sólo puedes ingresar hasta 100 caracteres")
+		}
+	}
+	messageForm();
+}
